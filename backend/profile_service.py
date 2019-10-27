e059a140b0482c29835ecfb5e99d90f217da63c3
Josh Levy <jml268@case.edu>
2019-10-26 21:22:20 -0400

    Added some stuff to get and create user profiles

--- backend/profile_service.py
+++ backend/profile_service.py
@@ -5,11 +5,51 @@ import db_code.Repository as db
 from flask import Blueprint, jsonify, request
 from flask_api import status
 
+from Model import User
+
 profile = Blueprint('profile', __name__)
 
 @profile.route('/profile')
 def getProfile():
-    if 'email' in request.args:
-        return db.getUser(request.args['email'])
+    """
+    Request: {"email" : <value>}
 
-    return {'invalid key': 'Can only search for users by email'}, status.HTTP_400_BAD_REQUEST
+    Response: {"user" : <obj>}
+    """
+    if 'email' not in request.args:
+        return {'invalid_key': 'Can only search for users by email'}, status.HTTP_400_BAD_REQUEST
+
+    return {'user': db.get_user(request.args['email']).__dict__}
+
+@profile.route('/login')
+def processLogin():
+    """
+    Request: {"email" : <value>}
+
+    Response: {"newUser" : <bool>, "user": <obj>}
+    """
+    if 'email' not in request.args:
+        return {'invalid_key': 'Can only search for users by email'}, status.HTTP_400_BAD_REQUEST
+
+    user = db.get_user(request.args['email'])
+    if user:
+        return {'newUser': False, 'user': user.__dict__}
+    else:
+        return {'newUser': True, 'user': None}
+
+@profile.route('/addUser')
+def add_user():
+    """
+    Request: {"user" : <obj>}
+
+    Response: empty
+    """
+    if 'email' not in request.args:
+        return {'invalid_user': 'User must have an email'}, status.HTTP_400_BAD_REQUEST
+
+    new_user = User(request.args['email'])
+    if db.get_user(new_user.email):
+        return {'already_exists': 'This user already exists'}, status.HTTP_400_BAD_REQUEST
+    
+    db.add_user(new_user)
+    return status.HTTP_200_OK
--- db_code/Repository.py
+++ db_code/Repository.py
@@ -1,9 +1,17 @@
 from pymongo import MongoClient
+
+import os, sys
+sys.path.append(os.path.join(os.path.dirname(sys.path[0] + 'backend')))
+import Model
+
 client = MongoClient('localhost')
 db = client.seatswap
 
-def getUser(email):
+def get_user(email):
     users = db.users
     result = users.find_one({'email': email})
-    del result['_id']
-    return result
+    return model.User(result)
+
+def add_user(user):
+    users = db.users
+    users.insert_one(user.__dict__)
