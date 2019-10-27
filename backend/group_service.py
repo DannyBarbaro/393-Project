ee5332cac77810bfc141a69c0fe285d52fc9fc4f
cmg144 <cmg144@case.edu>
2019-10-27 00:02:07 -0400

    back end things for groups

---
+++ backend/group_service.py
@@ -0,0 +1,67 @@
+from Model import Group
+from flask_api import status
+from flask import Blueprint, jsonify, request
+import db_code.Repository as db
+import os
+import sys
+sys.path.append(os.path.join(os.path.dirname(sys.path[0] + 'db_code')))
+
+
+group = Blueprint('group', __name__)
+
+
+@group.route('/createGroup')
+def createGroup():
+    """
+    Request: {"group" : <obj>}
+
+    Response: empty
+    """
+    if 'group' not in request.json:
+        return {'invalid_key': 'Invalid group creation request'}, status.HTTP_400_BAD_REQUEST
+    if 'owner' not in request.json['group']:
+        return {'no_owner': 'No owner in group request'}, status.HTTP_400_BAD_REQUEST
+    if 'event' not in request.json['group']:
+        return {'no_event': 'No event in group request'}, status.HTTP_400_BAD_REQUEST
+    
+    new_group = Group(request.json['group'])
+    if db.get_group(new_group.owner, new_group.event):
+        return {'group_exists': 'This group has been created already'}, status.HTTP_400_BAD_REQUEST
+    
+    db.add_group(new_group)
+    return status.HTTP_200_OK
+
+@group.route('/groups/<id>')
+def getGroup():
+    """
+    Request: {"id" : <id>}
+
+    Response: {"group" : <obj>}
+    """
+    if 'id' not in request.json:
+        return {'no_id': 'No group id in request'}, status.HTTP_400_BAD_REQUEST
+
+    group = db.get_group(request.json['id'])
+    if group:
+        return {'group': group.__dict__}
+    else:
+        return {'group': None}
+
+@group.route('/groups/<id>/join')
+def joinGroup():
+    """
+    Request: {"id" : <id>, "user" : <obj>}
+
+    Response: empty
+    """
+    if 'id' not in request.json:
+        return {'no_group_id': 'Group id missing from request'}, status.HTTP_400_BAD_REQUEST
+    if 'user' not in request.json:
+        return {'no_user': 'Joining user missing from request'}, status.HTTP_400_BAD_REQUEST
+
+    group = db.get_group(request.json['id'])
+    if group:
+        db.add_user_to_group(request.json['user'], group)
+        return status.HTTP_200_OK
+    else:
+        return {'group_not_found': 'Requested group could not be found'}, status.HTTP_400_BAD_REQUEST
--- db_code/Repository.py
+++ db_code/Repository.py
@@ -10,8 +10,28 @@ db = client.seatswap
 def get_user(email):
     users = db.users
     result = users.find_one({'email': email})
-    return model.User(result)
+    return Model.User(result)
 
 def add_user(user):
     users = db.users
     users.insert_one(user.__dict__)
+
+def get_group(id):
+    groups = db.groups
+    result = groups.find_one({'id': id})
+    return Model.Group(result)
+
+def get_group(owner, event):
+    groups = db.groups
+    result = groups.find_one({'owner': owner, 'event': event})
+    return Model.Group(result)
+
+def add_group(group):
+    groups = db.groups
+    groups.insert_one(group.__dict__)
+
+def add_user_to_group(user, group):
+    groups = db.groups
+    new_members = group.members
+    new_members.append(user)
+    groups.update_one({'id': group['id']}, {'$set': {'members': new_members}})
