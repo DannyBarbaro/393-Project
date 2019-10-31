88c5e159db11f20081bfbe4ff473823462c9cb21
cmg144 <cmg144@case.edu>
2019-10-31 12:36:57 -0400

    better way to get groups with user in it, but only if the $match and $in work like how I think they do

--- db_code/Repository.py
+++ db_code/Repository.py
@@ -58,6 +58,5 @@ def get_all_groups():
 
 def get_groups_with_user(user_id):
     groups = db.groups
-    group_ids = groups.aggregate({"$unwind": "$members"}, {"$match": {"members": user_id}}, {"$group": { "_id": "$_id"}})
-    user_groups = [groups.find_one({'id': group['id']})for group in group_ids]
+    user_groups = groups.aggregate({"$match": {"$in": {user_id, "$members"}}})
     return [Model.Group(g).__dict__ for g in user_groups]
