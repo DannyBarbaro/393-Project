c21ddf0f9640d7f898d6e4b54f33c2da06483598
cmg144 <cmg144@case.edu>
2019-11-18 00:48:18 -0500

    adds calling asserts for mocked functions

--- unit_tests/test_group_service.py
+++ unit_tests/test_group_service.py
@@ -8,17 +8,22 @@ import db_code.Repository as db
 import backend.group_service as service
 import pytest
 
+@pytest.fixture
+def mock_add_group(mocker):
+        return mocker.patch("Repository.add_group", return_value=None)
+
 class TestCreateGroup_NewGroup(object):
 
     @pytest.fixture
-    def app(self, mocker):
-        mocker.patch("Repository.get_group_by_owner",
-                     return_value=None)
-        mocker.patch("Repository.add_group", return_value=None)
-        app = create_app()
-        return app
+    def app(self):
+        return create_app()
 
-    def test_create_group_success(self, client):
+    @pytest.fixture
+    def mock_get_group(self, mocker):
+        return mocker.patch("Repository.get_group_by_owner",
+                            return_value=None)
+
+    def test_create_group_success(self, client, mock_get_group, mock_add_group):
         url = '/createGroup'
         data = {
             'group': {
@@ -29,9 +34,11 @@ class TestCreateGroup_NewGroup(object):
 
         response = client.post(url, json=data)
 
+        mock_get_group.assert_called()
+        mock_add_group.assert_called()
         assert response.status_code == 200
 
-    def test_create_group_no_group(self, client):
+    def test_create_group_no_group(self, client, mock_get_group):
         url = '/createGroup'
         data = {
             'notgroup': {
@@ -42,10 +49,11 @@ class TestCreateGroup_NewGroup(object):
 
         response = client.post(url, json=data)
 
+        mock_get_group.assert_not_called()
         assert response.json['invalid_key'] == 'Invalid group creation request'
         assert response.status_code == 400
 
-    def test_create_group_no_owner(self, client):
+    def test_create_group_no_owner(self, client, mock_get_group):
         url = '/createGroup'
         data = {
             'group': {
@@ -56,10 +64,11 @@ class TestCreateGroup_NewGroup(object):
 
         response = client.post(url, json=data)
 
+        mock_get_group.assert_not_called()
         assert response.json['no_owner'] == 'No owner in group request'
         assert response.status_code == 400
 
-    def test_create_group_no_event(self, client):
+    def test_create_group_no_event(self, client, mock_get_group):
         url = '/createGroup'
         data = {
             'group': {
@@ -70,6 +79,7 @@ class TestCreateGroup_NewGroup(object):
 
         response = client.post(url, json=data)
 
+        mock_get_group.assert_not_called()
         assert response.json['no_event'] == 'No event in group request'
         assert response.status_code == 400
 
@@ -77,14 +87,16 @@ class TestCreateGroup_NewGroup(object):
 class TestCreateGroup_AlreadyExists(object):
 
     @pytest.fixture
-    def app(self, mocker):
-        group = Group({'owner': 4, 'event': 3})
-        mocker.patch("Repository.get_group_by_owner",
-                     return_value=group)
-        app = create_app()
-        return app
+    def app(self):
+        return create_app()
 
-    def test_create_group_already_exists(self, client):
+    @pytest.fixture
+    def mock_get_group(self, mocker):
+        group = Group({'owner': 4, 'event': 3})
+        return mocker.patch("Repository.get_group_by_owner",
+                     return_value=group)
+
+    def test_create_group_already_exists(self, client, mock_get_group, mock_add_group):
         url = '/createGroup'
         data = {
             'group': {
@@ -95,6 +107,8 @@ class TestCreateGroup_AlreadyExists(object):
 
         response = client.post(url, json=data)
 
+        mock_get_group.assert_called()
+        mock_add_group.assert_not_called()
         assert response.json['group_exists'] == 'This group has been created already'
         assert response.status_code == 400
 
