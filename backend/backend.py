6f19712c502bb0669f9fae7b2c6e1faddeca0b94
cmg144 <cmg144@case.edu>
2019-12-04 02:13:55 -0500

    adds service call to get future events

--- backend/backend.py
+++ backend/backend.py
@@ -5,6 +5,7 @@ from flask_cors import CORS
 import profile_service as profile
 import group_service as groups
 import schedule_service as schedules
+import event_service as events
 # import blueprint files
 # ex: from Blueprint import test_api
 
@@ -20,6 +21,7 @@ def create_app():
     app.register_blueprint(profile.profile)
     app.register_blueprint(groups.group)
     app.register_blueprint(schedules.schedule)
+    app.register_blueprint(events.event)
 
     return app
 
