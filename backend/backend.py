ccd1be281c4065735c1ba7a5b77a8e24cff7d5b9
DannyBarbaro <dxb486@case.edu>
2019-12-03 23:09:14 -0500

    this might be important

---
+++ backend/backend.py
@@ -0,0 +1,28 @@
+import configparser
+from flask import Flask
+from flask_cors import CORS
+
+import profile_service as profile
+import group_service as groups
+# import blueprint files
+# ex: from Blueprint import test_api
+
+# Read configuration from file.
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+# Set up application serverGET.
+def create_app():
+    app = Flask(__name__)
+    CORS(app)
+
+    app.register_blueprint(profile.profile)
+    app.register_blueprint(groups.group)
+
+    return app
+
+
+app = create_app()
+
+if __name__ == '__main__':
+    app.run(**config['app'])
