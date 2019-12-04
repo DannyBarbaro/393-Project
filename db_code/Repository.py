e09f40753e1555699c8217eaff6700a1338f791a
cmg144 <cmg144@case.edu>
2019-12-04 02:27:50 -0500

    names

--- db_code/Repository.py
+++ db_code/Repository.py
@@ -125,7 +125,7 @@ def get_all_events():
 def get_all_future_events():
     events = db.events
     now = datetime.utcnow()
-    future_events = events.aggregate([{"$match": {'time': {'$gt': now}}}])
+    future_events = events.aggregate([{"$match": {'start_time': {'$gt': now}}}])
     return [Model.Event(e) for e in future_events]
 
 def get_event_by_id(event_id):
